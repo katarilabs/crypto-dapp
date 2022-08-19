32b870f88bd3ab08713f4a27bbbf53cc2b500809
Jhymer Martínez <jhmartinez1991@gmail.com>
2022-08-19 16:37:31 -0500

    add dotenv package to read from a .env file

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
-Try running some of the following tasks:
+## 1. Create a `.env` file in the root, please use the `.env.example` file like a base template
+
+## 2. Try running some of the following tasks:
 
 ```shell
 yarn hardhat help
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
+
+const {
+  DEPLOYER_SIGNER_PRIVATE_KEY = '',
+  INFURA_PROJECT_ID = ''
+} = process.env
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [
+        DEPLOYER_SIGNER_PRIVATE_KEY
+      ]
+    }
+  }
 };
 
 export default config;
